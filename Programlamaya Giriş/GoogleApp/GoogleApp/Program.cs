cc90ad3c5f243ca093175c06a98fda53c9e3e162
Ali Kemal Çalak <kemalcalak@gmail.com>
2023-12-10 18:02:55 +0300

    Programlamaya giriş

---
+++ Programlamaya Giriş/GoogleApp/GoogleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Hello World!");
+            Console.Write("Yenisatir");
+            Console.Write("DigerWrite");
+            Console.ReadLine();
+            */
+            /*
+            Console.WriteLine("Bu bir satirdir");
+            Console.WriteLine("Bu da bir satirdir");
+            Console.ReadLine();
+            */
+            Console.Write("Bu bir satirdir");
+            Console.Write("\nBu da bir satirdir");
+            Console.ReadLine();
+        }
+    }
+}

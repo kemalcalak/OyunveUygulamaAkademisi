507f0ac4c657bc3b659693a1b996592d64f94240
Ali Kemal Çalak <kemalcalak@gmail.com>
2023-12-10 18:03:16 +0300

    Değerlerin kullanılması

---
+++ Değerlerin Kullanılması/GoogleApp/GoogleApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleApp
+{
+    class Program
+    {
+        /*
+        static char e;
+        static string d;
+        static int a;
+        static float b;
+        static bool c;
+        */
+
+        static void Main(string[] args)
+        {
+            /*
+            e = 'a';
+            d = "Hello World";
+            c = true;
+            b = 1.1f;
+            a = 1;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.ReadLine();
+            */
+            /*
+            var a = 1;
+            var b = 1.1f;
+            var c = true;
+            var d = "Hello World";
+            var e = 'a';
+            float ondalikUcBasamakliSayi; //camel case
+            float OndalikSayi; //pascal case
+            string benimAdim;
+            benimAdim = "AliKemal";
+            Console.WriteLine(benimAdim);
+            benimAdim = "Ali";
+            Console.WriteLine(benimAdim);
+            Console.ReadLine();
+            */
+            string a="İzmir'de bomba yedim ve tanesi";
+            string b = "liraydı ve sıcaklığı";
+            string c = "dereceydi.";
+            int d = 5;
+            float e = 25.5f;
+            Console.WriteLine(a + ' ' + d + ' ' + b + ' ' + e + ' ' +c);
+            Console.ReadLine();
+        }
+    }
+}
